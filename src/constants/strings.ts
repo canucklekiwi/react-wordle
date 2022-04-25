69b10c5a200616de3328f0d91f6b9ba3459a0a6d
canucklekiwi <102553463+canucklekiwi@users.noreply.github.com>
2022-04-26 10:02:34 +1200

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,6 +1,6 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['You really know Mae!', 'Awesome, you must know Mae!', 'Mae-hey-hey!!']
+export const WIN_MESSAGES = ['You are very clever!', 'Awesome!', 'Hey-hey!!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
@@ -17,11 +17,11 @@ export const NOT_CONTAINED_MESSAGE = (letter: string) =>
   `Guess must contain ${letter}`
 export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Mae Statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'Mae Guess Distribution'
-export const NEW_WORD_TEXT = 'New Mae word in'
-export const SHARE_TEXT = 'Share-Mae'
-export const TOTAL_TRIES_TEXT = 'Total Mae tries'
-export const SUCCESS_RATE_TEXT = 'Mae Success rate'
-export const CURRENT_STREAK_TEXT = 'Current Mae streak'
-export const BEST_STREAK_TEXT = 'Best Mae streak'
+export const STATISTICS_TITLE = 'Statistics'
+export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
+export const NEW_WORD_TEXT = 'New word in'
+export const SHARE_TEXT = 'Share'
+export const TOTAL_TRIES_TEXT = 'Total tries'
+export const SUCCESS_RATE_TEXT = 'Success rate'
+export const CURRENT_STREAK_TEXT = 'Current streak'
+export const BEST_STREAK_TEXT = 'Best streak'
