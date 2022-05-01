169492eb3273182884e0c13a9b6dccdceac93f38
canucklekiwi <102553463+canucklekiwi@users.noreply.github.com>
2022-05-01 21:32:05 +1200

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,6 +1,6 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['You are very clever!', 'Awesome!', 'Hey-hey!!']
+export const WIN_MESSAGES = ['Clever Alonzo!', 'Awesome Alonzo!', 'Hey-hey that''s Alonzo!!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
